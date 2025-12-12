b73169a27957470ecb7268d2877afb3331cfed44
Yudai-ASANO <yuudai.asano@link-u.co.jp>
2025-12-12 14:04:08 +0900

    Initial Commit

---
+++ ContainerRelativeFrame/ContainerRelativeFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerRelativeFrameApp.swift
+//  ContainerRelativeFrame
+//
+//  Created by Yudai Kanaoka on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct ContainerRelativeFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
